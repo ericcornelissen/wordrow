412c8902b0d581a0684f5f862ae41754c459b99c
Eric Cornelisesn <ericornelissen@gmail.com>
2020-04-08 11:42:18 +0300

    Remove helper functions in logger package
    Remove the helper functions in the logger package by calling the standard logging function (Error, Fatal, Info, Warning) from the respective format loggin function (Errorf, Fatalf, Infof, Warningf) instead of using a helper function for each of these.

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -9,61 +9,41 @@ func toSentenceCase(s string) string {
   return strings.ToUpper(s[:1]) + s[1:]
 }
 
-// Print a message as an error message.
-func printError(msg string) {
-  fmt.Printf("[E] %s", toSentenceCase(msg))
-}
-
-// Print a message as a fatal message.
-func printFatal(msg string) {
-  fmt.Printf("[F] %s", toSentenceCase(msg))
-}
-
-// Print a message as an info message.
-func printInfo(msg string) {
-  fmt.Printf("[I] %s", toSentenceCase(msg))
-}
-
-// Print a message as a warning message.
-func printWarning(msg string) {
-  fmt.Printf("[W] %s", toSentenceCase(msg))
-}
-
 
 // Print a set of messages as an error message.
 func Error(msgs ...interface{}) {
   msg := fmt.Sprintln(msgs...)
-  printError(msg)
+  fmt.Printf("[E] %s", toSentenceCase(msg))
 }
 
 // Print and format a message as an error message.
 func Errorf(msg string, args ...interface{}) {
   formattedMsg := fmt.Sprintf(msg, args...)
-  printError(formattedMsg + "\n")
+  Error(formattedMsg + "\n")
 }
 
 // Print a set of messages as a fatal message.
 func Fatal(msgs ...interface{}) {
   msg := fmt.Sprintln(msgs...)
-  printFatal(msg)
+  fmt.Printf("[F] %s", toSentenceCase(msg))
 }
 
 // Print and format a message as a fatal message.
 func Fatalf(msg string, args ...interface{}) {
   formattedMsg := fmt.Sprintf(msg, args...)
-  printFatal(formattedMsg + "\n")
+  Fatal(formattedMsg + "\n")
 }
 
 // Print a set of messages as an info message.
 func Info(msgs ...interface{}) {
   msg := fmt.Sprintln(msgs...)
-  printInfo(msg)
+  fmt.Printf("[I] %s", toSentenceCase(msg))
 }
 
 // Print and format a message as an info message.
 func Infof(msg string, args ...interface{}) {
   formattedMsg := fmt.Sprintf(msg, args...)
-  printInfo(formattedMsg + "\n")
+  Info(formattedMsg + "\n")
 }
 
 // Print a set of messages.
@@ -79,11 +59,11 @@ func Printf(msg string, args ...interface{}) {
 // Print a set of messages as a warning message.
 func Warning(msgs ...interface{}) {
   msg := fmt.Sprintln(msgs...)
-  printWarning(msg)
+  fmt.Printf("[W] %s", toSentenceCase(msg))
 }
 
 // Print and format a message as a warning message.
 func Warningf(msg string, args ...interface{}) {
   formattedMsg := fmt.Sprintf(msg, args...)
-  printWarning(formattedMsg + "\n")
+  Warning(formattedMsg + "\n")
 }
