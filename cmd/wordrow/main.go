8f4ad6642b6b683fe0e92a9b072e772e314e053f
Eric Cornelisesn <ericornelissen@gmail.com>
2020-05-03 23:43:42 +0300

    Proof of concept of reading from STDIN and writing to STDIN
    Simple, proof of concept implementation of wordrow working on the input 
    from STDIN, fixing it based on the specified arguments, and outputting 
    the result to STDOUT.

--- cmd/wordrow/main.go
+++ cmd/wordrow/main.go
@@ -1,5 +1,6 @@
 package main
 
+import "bufio"
 import "os"
 import "strings"
 
@@ -9,6 +10,18 @@ import "github.com/ericcornelissen/wordrow/internal/fs"
 import "github.com/ericcornelissen/wordrow/internal/logger"
 import "github.com/ericcornelissen/wordrow/internal/replacer"
 import "github.com/ericcornelissen/wordrow/internal/wordmaps"
+
+// Check if the program received input from STDIN.
+//
+// based on: https://stackoverflow.com/a/38612652
+func hasStdin() bool {
+	fi, err := os.Stdin.Stat()
+	if err != nil {
+		return false
+	}
+
+	return (fi.Mode() & os.ModeNamedPipe) != 0
+}
 
 func getWordMap(
 	mapFilesPaths []string,
@@ -73,6 +86,22 @@ func run(args cli.Arguments) error {
 	return nil
 }
 
+func runOnStdin(args cli.Arguments, input string) error {
+	wm, err := getWordMap(args.MapFiles, args.Mappings)
+	if err != nil {
+		return err
+	}
+
+	if args.Invert {
+		wm.Invert()
+	}
+
+	fixedInput := replacer.ReplaceAll(input, wm)
+	os.Stdout.WriteString(fixedInput)
+
+	return nil
+}
+
 func setLogLevel(args cli.Arguments) {
 	if args.Silent {
 		logger.SetLogLevel(logger.ERROR)
@@ -83,7 +112,25 @@ func setLogLevel(args cli.Arguments) {
 
 func main() {
 	shouldRun, args := cli.ParseArgs(os.Args)
-	if shouldRun {
+
+	if hasStdin() {
+		logger.SetLogLevel(logger.FATAL)
+
+		var sb strings.Builder
+
+		scanner := bufio.NewScanner(os.Stdin)
+		for scanner.Scan() {
+			line := scanner.Text()
+			sb.WriteString(line)
+			sb.WriteByte('\n')
+		}
+
+		input := sb.String()
+		err := runOnStdin(args, input)
+		if err != nil {
+			panic(err)
+		}
+	} else if shouldRun {
 		setLogLevel(args)
 
 		err := run(args)
