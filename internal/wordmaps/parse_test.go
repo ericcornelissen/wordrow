ericcornelissen/wordrow

commit 028fe5a916b1b85cc6a53234bd4238038584b15c

Add support to explicitly state mapping format from CLI (#46)
* Proof of concept of CLI specified mapping format separate from extension

Simple proof of concept (not-working) implementation that allows users 
to specify the format of a mapping file explicitly from the CLI, 
overriding the default format assumed from the file's extension.

The implementation is not working as the new function in the wordmaps 
package does not yet actually use the provided format.

* Refactor main

Extract logic to create a WordMap based on the input arguments from 
main.go and into a separate wordmap.go file in the main package. Also, 
the refactoring includes a small changes to the `getWordMap` function, 
simplying it just slightly.

To be able to do this the build target of the Makefile had to be 
updated.

In addition the unintentional change in wordmaps/csv.go is reverted.

* Break `getWordMap` into two separate functions

Previously this function contained the code to process both map files 
and CLI specified mappings. These two functionalities are now exracted 
into helper functions called by `getWordMap`.

In addition, the logging in this package has been updated.

* Simplify usage of `parseWordMapArgument`

* Update CHANGELOG

cmd/wordrow/wordmap.go
@@ -1,0 +1,72 @@
+package main
+
+import "strings"
+
+import "github.com/ericcornelissen/wordrow/internal/errors"
+import "github.com/ericcornelissen/wordrow/internal/fs"
+import "github.com/ericcornelissen/wordrow/internal/logger"
+import "github.com/ericcornelissen/wordrow/internal/wordmaps"
+
+// Parse a --map-file argument into its component parts.
+func parseWordMapArgument(argument string) (path string, format string) {
+	fileExtension := fs.GetExt(path)
+
+	tmp := strings.Split(fileExtension, ":")
+	if len(tmp) > 1 {
+		format := tmp[len(tmp)-1]
+		return strings.TrimSuffix(argument, ":"+format), format
+	}
+
+	return argument, fileExtension
+}
+
+// Add the mappings defined in the specified map files to the WordMap.
+func processMapFiles(mapFilesArgs []string, wm *wordmaps.WordMap) error {
+	for _, mapFileArg := range mapFilesArgs {
+		logger.Debugf("Parsing argument '%s'", mapFileArg)
+		file, format := parseWordMapArgument(mapFileArg)
+
+		logger.Debugf("Reading '%s'", file)
+		mapFile, err := fs.ReadFile(file)
+		if err != nil {
+			return err
+		}
+
+		logger.Debugf("Processing '%s' as a %s mapping file", mapFile.Path, format)
+		err = wm.AddFile(&mapFile.Content, format)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// Add the mappings defined in the CLI to the WordMap.
+func processMappings(mappings []string, wm *wordmaps.WordMap) error {
+	for _, mapping := range mappings {
+		logger.Debugf("Processing CLI specified mapping: '%s'", mapping)
+		values := strings.Split(mapping, ",")
+		if len(values) != 2 {
+			return errors.Newf("Incorrect mapping from CLI: '%s'", mapping)
+		}
+
+		wm.AddOne(values[0], values[1])
+	}
+
+	return nil
+}
+
+// Get a WordMap based on the specified mappings and map files.
+func getWordMap(
+	mapFilesArgs []string,
+	cliMappings []string,
+) (wordmap wordmaps.WordMap, err error) {
+	logger.Debug("Processing CLI specified map files...")
+	err = processMapFiles(mapFilesArgs, &wordmap)
+
+	logger.Debug("Processing CLI specified mappings...")
+	err = processMappings(cliMappings, &wordmap)
+
+	return wordmap, err
+}

Makefile
@@ -4,7 +4,7 @@ test_root=./internal/...
 
 
 build:
-	go build -o $(executable_file) cmd/wordrow/main.go
+	go build -o $(executable_file) ./cmd/wordrow/
 
 test:
 	go test $(test_root)

cmd/wordrow/main.go
@@ -1,47 +1,11 @@
 package main
 
 import "os"
-import "strings"
 
 import "github.com/ericcornelissen/wordrow/internal/cli"
-import "github.com/ericcornelissen/wordrow/internal/errors"
 import "github.com/ericcornelissen/wordrow/internal/fs"
 import "github.com/ericcornelissen/wordrow/internal/logger"
 import "github.com/ericcornelissen/wordrow/internal/replacer"
-import "github.com/ericcornelissen/wordrow/internal/wordmaps"
-
-func getWordMap(
-	mapFilesPaths []string,
-	cliMappings []string,
-) (wm wordmaps.WordMap, err error) {
-	logger.Debug("Reading specified mapping files")
-	mapFiles, err := fs.ReadFiles(mapFilesPaths)
-	if err != nil {
-		return wm, err
-	}
-
-	for _, mapFile := range mapFiles {
-		logger.Debugf("Processing '%s' as mapping file", mapFile.Path)
-		err := wm.AddFile(&mapFile.Content, mapFile.Ext)
-		if err != nil {
-			return wm, err
-		}
-	}
-
-	logger.Debug("Processing CLI specified mappings")
-	for _, mapping := range cliMappings {
-		logger.Debugf("Processing CLI specified mapping: '%s'", mapping)
-
-		values := strings.Split(mapping, ",")
-		if len(values) != 2 {
-			return wm, errors.Newf("Incorrect mapping from CLI: '%s'", mapping)
-		}
-
-		wm.AddOne(values[0], values[1])
-	}
-
-	return wm, err
-}
 
 func run(args cli.Arguments) error {
 	wm, err := getWordMap(args.MapFiles, args.Mappings)

commit 660d1bcd8b8a7bae5a2442631fa528d2ee3e8d7f

Make mapping file formats case insenstive (#55)
* Fix loading mapping files based on extension

Fix accidental bug introduced in #46 due to using the wrong variable 
name to get the file extension in main' wordmap file.

* Make format name case insensitive

Update the regular expressions defining valid format names to be case 
insensitive, allowing file extensions (and explicit mapping types) to be, 
e.g., all caps.

* Update CHANGELOG

cmd/wordrow/wordmap.go
@@ -9,7 +9,7 @@ import "github.com/ericcornelissen/wordrow/internal/wordmaps"
 
 // Parse a --map-file argument into its component parts.
 func parseWordMapArgument(argument string) (path string, format string) {
-	fileExtension := fs.GetExt(path)
+	fileExtension := fs.GetExt(argument)
 
 	tmp := strings.Split(fileExtension, ":")
 	if len(tmp) > 1 {

internal/wordmaps/parse.go
@@ -6,10 +6,10 @@ import "github.com/ericcornelissen/wordrow/internal/errors"
 
 var (
 	// Regular expression of names considered as MarkDown format.
-	md = regexp.MustCompile(`\.(md(te?xt)?|markdown|mdown|mkdown|mkd|mdwn|mkdn)`)
+	md = regexp.MustCompile(`(?i)\.(md(te?xt)?|markdown|mdown|mkdown|mkd|mdwn|mkdn)`)
 
 	// Regular expression of names considered as CSV format.
-	csv = regexp.MustCompile(`csv`)
+	csv = regexp.MustCompile(`(?i)\.csv`)
 )
 
 // A parse function is a function that takes the contents of a file as a string

internal/wordmaps/parse_test.go
@@ -27,6 +27,10 @@ func TestGetParserForMarkDownFile(t *testing.T) {
 		parseFn, err := getParserForFormat(".md")
 		check(parseFn, err)
 	})
+	t.Run(".MD", func(t *testing.T) {
+		parseFn, err := getParserForFormat(".MD")
+		check(parseFn, err)
+	})
 	t.Run(".mdown", func(t *testing.T) {
 		parseFn, err := getParserForFormat(".mdown")
 		check(parseFn, err)
@@ -54,16 +58,25 @@ func TestGetParserForMarkDownFile(t *testing.T) {
 }
 
 func TestGetParserForCSVFile(t *testing.T) {
-	parseFn, err := getParserForFormat(".csv")
+	check := func(parseFn parseFunction, err error) {
+		if err != nil {
+			t.Fatalf("The error should be nil for this test (Error: %s)", err)
+		}
 
-	if err != nil {
-		t.Fatalf("The error should be nil for this test (Error: %s)", err)
+		actual, expected := reflect.ValueOf(parseFn), reflect.ValueOf(parseCsvFile)
+		if actual.Pointer() != expected.Pointer() {
+			t.Error("The parser function should be the CSV parse function")
+		}
 	}
 
-	actual, expected := reflect.ValueOf(parseFn), reflect.ValueOf(parseCsvFile)
-	if actual.Pointer() != expected.Pointer() {
-		t.Error("The parser function should be the CSV parse function")
-	}
+	t.Run(".csv", func(t *testing.T) {
+		parseFn, err := getParserForFormat(".csv")
+		check(parseFn, err)
+	})
+	t.Run(".CSV", func(t *testing.T) {
+		parseFn, err := getParserForFormat(".CSV")
+		check(parseFn, err)
+	})
 }
 
 func TestParseFileNoParser(t *testing.T) {