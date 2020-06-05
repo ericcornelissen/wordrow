dd6fc085e492ea266b12bf6c5e6dbce4c4fba57b
Eric Cornelisesn <ericornelissen@gmail.com>
2020-06-05 14:21:05 +0300

    Add CSV support to map multiple values to one word

--- internal/wordmaps/csv.go
+++ internal/wordmaps/csv.go
@@ -2,23 +2,48 @@ package wordmaps
 
 import "strings"
 
+// TrimSpaceAll trims leading and trailing spaces in all strings. Note that this
+// function operates in place.
+func trimSpaceAll(v []string) {
+	for i, s := range v {
+		v[i] = strings.TrimSpace(s)
+	}
+}
+
+// AnyString returns true if at least one item in the list fulfills the
+// condition and false otherwise.
+func anyString(v []string, condition func(string) bool) bool {
+	for _, s := range v {
+		if condition(s) {
+			return true
+		}
+	}
+
+	return false
+}
+
+// IsEmptyString returns true if the string is empty and false otherwise.
+func isEmptyString(s string) bool {
+	return s == ""
+}
+
 // Parse a single row of a CSV file and add it to the WordMap.
 //
 // The error will be set if the row has an unexpected format, for example an
 // incorrect number of columns.
 func parseRow(row string, wm *WordMap) error {
 	rowValues := strings.Split(row, ",")
-	if len(rowValues) != 2 {
+	if len(rowValues) < 2 {
 		return &parseError{"Unexpected row format", row}
 	}
 
-	fromValue := strings.TrimSpace(rowValues[0])
-	toValue := strings.TrimSpace(rowValues[1])
-	if fromValue == "" || toValue == "" {
+	trimSpaceAll(rowValues)
+	if anyString(rowValues, isEmptyString) {
 		return &parseError{"Missing value", row}
 	}
 
-	wm.AddOne(fromValue, toValue)
+	last := len(rowValues) - 1
+	wm.AddMany(rowValues[0:last], rowValues[last])
 	return nil
 }
 
--- internal/wordmaps/csv_test.go
+++ internal/wordmaps/csv_test.go
@@ -5,6 +5,80 @@ import (
 	"testing"
 )
 
+func TestTrimAll(t *testing.T) {
+	subject := []string{
+		"a",
+		" b",
+		"c ",
+		" d ",
+	}
+
+	trimSpaceAll(subject)
+
+	if subject[0] != "a" {
+		t.Errorf("Incorrect first value (got '%s')", subject[0])
+	}
+
+	if subject[1] != "b" {
+		t.Errorf("Incorrect second value (got '%s')", subject[1])
+	}
+
+	if subject[2] != "c" {
+		t.Errorf("Incorrect third value (got '%s')", subject[2])
+	}
+
+	if subject[3] != "d" {
+		t.Errorf("Incorrect fourth value (got '%s')", subject[3])
+	}
+}
+
+func TestAnyString(t *testing.T) {
+	condition := func(s string) bool {
+		return s == ""
+	}
+
+	t.Run("empty list", func(t *testing.T) {
+		result := anyString([]string{}, condition)
+
+		if result == true {
+			t.Error("Unexpected result `true` for empty list")
+		}
+	})
+	t.Run("condition never holds", func(t *testing.T) {
+		result := anyString([]string{"a", "b", "c"}, condition)
+
+		if result == true {
+			t.Error("Unexpected result `true` for list")
+		}
+	})
+	t.Run("condition holds once", func(t *testing.T) {
+		result := anyString([]string{"a", "", "c"}, condition)
+
+		if result == false {
+			t.Error("Unexpected result `false` for list")
+		}
+	})
+	t.Run("condition holds often", func(t *testing.T) {
+		result := anyString([]string{"", "", "c"}, condition)
+
+		if result == false {
+			t.Error("Unexpected result `false` for list")
+		}
+	})
+}
+
+func TestIsEmptyString(t *testing.T) {
+	result := isEmptyString("")
+	if result == false {
+		t.Error("Unexpected result `false` for empty string")
+	}
+
+	result = isEmptyString("Hello world!")
+	if result == true {
+		t.Error("Unexpected result `true` for non-empty string")
+	}
+}
+
 func TestCsvOneRow(t *testing.T) {
 	csv := `cat,dog`
 	wm, err := parseCsvFile(&csv)
@@ -59,6 +133,39 @@ func TestCsvMultipleRows(t *testing.T) {
 	}
 
 	actual, expected = wm.GetTo(1), "zebra"
+	if actual != expected {
+		t.Errorf("Incorrect to-value at index 0 (actual %s)", actual)
+	}
+}
+
+func TestCsvManyColumns(t *testing.T) {
+	csv := `cat,dog,horse`
+	wm, err := parseCsvFile(&csv)
+
+	if err != nil {
+		t.Fatalf("Error should be nil for this test (Error: %s)", err)
+	}
+
+	if wm.Size() != 2 {
+		t.Fatalf("The WordMap size should be 2 (was %d)", wm.Size())
+	}
+
+	actual, expected := wm.GetFrom(0), "cat"
+	if actual != expected {
+		t.Errorf("Incorrect from-value at index 0 (actual %s)", actual)
+	}
+
+	actual, expected = wm.GetTo(0), "horse"
+	if actual != expected {
+		t.Errorf("Incorrect to-value at index 0 (actual %s)", actual)
+	}
+
+	actual, expected = wm.GetFrom(1), "dog"
+	if actual != expected {
+		t.Errorf("Incorrect from-value at index 0 (actual %s)", actual)
+	}
+
+	actual, expected = wm.GetTo(1), "horse"
 	if actual != expected {
 		t.Errorf("Incorrect to-value at index 0 (actual %s)", actual)
 	}
@@ -160,19 +267,6 @@ func TestCsvIgnoresWhitespaceInRow(t *testing.T) {
 
 func TestCsvToFewColumns(t *testing.T) {
 	csv := `zebra`
-	_, err := parseCsvFile(&csv)
-
-	if err == nil {
-		t.Fatal("Error should be set for incorrect CSV file")
-	}
-
-	if !strings.Contains(err.Error(), "Unexpected row") {
-		t.Errorf("Incorrect error message for (actual '%s')", err)
-	}
-}
-
-func TestCsvToManyColumns(t *testing.T) {
-	csv := `cat,dog,horse`
 	_, err := parseCsvFile(&csv)
 
 	if err == nil {
