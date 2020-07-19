8d173f8f7c50d2dbbf67cd740e077d16eec6b6bd
Eric Cornelissen <ericornelissen@gmail.com>
2020-07-19 18:50:21 +0300

    Refactor main/wordmap.go to use updated fs package
    And some general improvements to structure, readability and
    documentation.

--- cmd/wordrow/wordmap.go
+++ cmd/wordrow/wordmap.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"io/ioutil"
+
 	"github.com/ericcornelissen/wordrow/internal/errors"
 	"github.com/ericcornelissen/wordrow/internal/fs"
 	"github.com/ericcornelissen/wordrow/internal/logger"
@@ -9,32 +11,68 @@ import (
 )
 
 // Parse a --map-file argument into its component parts.
-func parseWordMapArgument(argument string) (path string, format string) {
+//
+// A --map-file argument can either be just a file path, or a file path with
+// ":format" appended to it. For example:
+//
+//   /path/to/file.csv
+//   /path/to/file.txt:csv
+//
+// In the former the file extension is returned as format, in the latter the
+// explicitly stated format is returned as format.
+func parseMapFileArgument(argument string) (filePath string, format string) {
 	fileExtension := fs.GetExt(argument)
 
 	tmp := strings.Split(fileExtension, ":")
 	if len(tmp) > 1 {
-		format := tmp[len(tmp)-1]
-		return strings.TrimSuffix(argument, ":"+format), format
+		explicitFormat := tmp[len(tmp)-1]
+		filePath := strings.TrimSuffix(argument, ":"+explicitFormat)
+		return filePath, explicitFormat
 	}
 
 	return argument, fileExtension
 }
 
-// Add the mappings defined in the specified map files to the WordMap.
-func processMapFiles(mapFilesArgs []string, wm *wordmaps.WordMap) error {
-	for _, mapFileArg := range mapFilesArgs {
-		logger.Debugf("Parsing argument '%s'", mapFileArg)
-		file, format := parseWordMapArgument(mapFileArg)
+// Add the mapping of a map file to the `wordmap`. The `format` argument
+// determines how the contents of the file are parsed.
+func processMapFile(
+	reader fs.Reader,
+	format string,
+	wordmap *wordmaps.WordMap,
+) error {
+	data, err := ioutil.ReadAll(reader)
+	if err != nil {
+		return err
+	}
 
-		logger.Debugf("Reading '%s'", file)
-		mapFile, err := fs.ReadFile(file)
-		if err != nil {
-			return err
-		}
+	content := string(data)
+	return wordmap.AddFile(&content, format)
+}
 
-		logger.Debugf("Processing '%s' as a %s mapping file", mapFile.Path, format)
-		err = wm.AddFile(&mapFile.Content, format)
+// Open a map file and add its mapping to the `wordmap`.
+func openAndProcessMapFile(
+	fileArgument string,
+	wordmap *wordmaps.WordMap,
+) error {
+	filePath, format := parseMapFileArgument(fileArgument)
+
+	logger.Debugf("Opening '%s' as a '%s' formatted map file", filePath, format)
+	handle, err := fs.OpenFile(filePath, fs.OReadOnly)
+	if err != nil {
+		return errors.Newf("Could not open '%s' in %s mode", filePath, fs.OReadOnly)
+	}
+
+	defer handle.Close()
+	return processMapFile(handle, format, wordmap)
+}
+
+// Open the specified map files and add their mapping to the `wordmap`.
+func openAndProcessMapFiles(
+	filePaths []string,
+	wordmap *wordmaps.WordMap,
+) error {
+	for _, filePath := range filePaths {
+		err := openAndProcessMapFile(filePath, wordmap)
 		if err != nil {
 			return err
 		}
@@ -43,13 +81,16 @@ func processMapFiles(mapFilesArgs []string, wm *wordmaps.WordMap) error {
 	return nil
 }
 
-// Add the mappings defined in the CLI to the WordMap.
-func processMappings(mappings []string, wm *wordmaps.WordMap) error {
+// Add the CLI defined mappings to the `wordmap`.
+func processInlineMappings(
+	mappings []string,
+	wm *wordmaps.WordMap,
+) error {
 	for _, mapping := range mappings {
-		logger.Debugf("Processing CLI specified mapping: '%s'", mapping)
+		logger.Debugf("Processing the CLI defined mapping '%s'", mapping)
 		values := strings.Split(mapping, ",")
 		if len(values) != 2 {
-			return errors.Newf("Incorrect mapping from CLI: '%s'", mapping)
+			return errors.Newf("Invalid CLI defined mapping '%s'", mapping)
 		}
 
 		wm.AddOne(values[0], values[1])
@@ -58,18 +99,16 @@ func processMappings(mappings []string, wm *wordmaps.WordMap) error {
 	return nil
 }
 
-// Get a WordMap based on the specified mappings and map files.
+// Get a WordMap for the specified map files and inline mappings.
 func getWordMap(
-	mapFilesArgs []string,
-	cliMappings []string,
+	mapFiles []string,
+	inlineMappings []string,
 ) (wordmap wordmaps.WordMap, err error) {
-	logger.Debug("Processing CLI specified map files...")
-	err = processMapFiles(mapFilesArgs, &wordmap)
+	err = openAndProcessMapFiles(mapFiles, &wordmap)
 	if err != nil {
 		return wordmap, err
 	}
 
-	logger.Debug("Processing CLI specified mappings...")
-	err = processMappings(cliMappings, &wordmap)
+	err = processInlineMappings(inlineMappings, &wordmap)
 	return wordmap, err
 }
