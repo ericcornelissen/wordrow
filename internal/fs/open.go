dd98b235e7f6cbb60130485fa254c8b1f4d83931
Eric Cornelisesn <ericornelissen@gmail.com>
2020-07-24 12:04:00 +0300

    Mitigate gosec G304 error in fs.OpenFile
    As per the (current) gosec docs [1] ruel G304 warns about:
    
    > File path provided as taint input
    
    Following the gosec guidelines [2] this adds a call to `filePath.Clean`
    before opneing the specified file.
    
    --
    1. https://github.com/securego/gosec/tree/6bcd89aa6b1cc8a448f99d51886ac29703eb1804#available-rules
    2. https://securego.io/docs/rules/g304.html

--- internal/fs/open.go
+++ internal/fs/open.go
@@ -1,6 +1,9 @@
 package fs
 
-import "os"
+import (
+	"os"
+	"path/filepath"
+)
 
 // The default mode used by OpenFile.
 const mode = 0600
@@ -8,7 +11,8 @@ const mode = 0600
 // OpenFile opens a File handle to interact with the file specified by
 // `filePath`.
 func OpenFile(filePath string, flag Flag) (file *File, err error) {
-	osFile, err := os.OpenFile(filePath, flagToFlag(flag), mode)
+	cleanFilePath := filepath.Clean(filePath)
+	osFile, err := os.OpenFile(cleanFilePath, flagToFlag(flag), mode)
 	return &File{
 		handle: osFile,
 		path:   filePath,
