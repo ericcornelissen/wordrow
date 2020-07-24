98e32f471f9517686f88df6739ab51701aa53577
Eric Cornelisesn <ericornelissen@gmail.com>
2020-07-24 12:12:24 +0300

    Inline filePath.Clean in OpenFile call

--- internal/fs/open.go
+++ internal/fs/open.go
@@ -11,8 +11,7 @@ const mode = 0600
 // OpenFile opens a File handle to interact with the file specified by
 // `filePath`.
 func OpenFile(filePath string, flag Flag) (file *File, err error) {
-	cleanFilePath := filepath.Clean(filePath)
-	osFile, err := os.OpenFile(cleanFilePath, flagToFlag(flag), mode)
+	osFile, err := os.OpenFile(filepath.Clean(filePath), flagToFlag(flag), mode)
 	return &File{
 		handle: osFile,
 		path:   filePath,
