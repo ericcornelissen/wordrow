acc6e160491cf63064c3d6fb8910db954139ec13
Eric Cornelisesn <ericornelissen@gmail.com>
2020-08-11 15:15:10 +0300

    Remove unused function getCwd
    Remove the unused function called `getCwd` in the fs package. This
    functions was found to be unused by the deadcode static analysis
    tool [1].
    
    --
    1. https://github.com/remyoudompheng/go-misc/tree/2d6ac652a50e368c874b15d00ea5e346e240e906/deadcode

--- internal/fs/paths.go
+++ internal/fs/paths.go
@@ -1,30 +1,15 @@
 package fs
 
 import (
-	"os"
 	"path/filepath"
 	"regexp"
 
 	"github.com/ericcornelissen/wordrow/internal/errors"
-	"github.com/ericcornelissen/wordrow/internal/logger"
 	"github.com/yargevad/filepathx"
 )
 
 // Regular expression for glob strings.
 var globExpr = regexp.MustCompile(`[\*\?\[\]]`)
-
-// Get the (current) working directory.
-//
-// The function panics if the (current) working directory could not be found.
-func getCwd() string {
-	cwd, err := os.Getwd()
-	if err != nil {
-		logger.Fatal("Current working directory could not be obtained")
-		panic(1)
-	}
-
-	return cwd
-}
 
 // GetExt returns the extension of a given file path.
 func GetExt(path string) string {
