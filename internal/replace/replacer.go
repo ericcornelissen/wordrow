3578d605a2e79747f550a4732137970e598efcf7
Eric Cornelissen <ericornelissen@gmail.com>
2020-12-17 17:04:03 +0100

    Remove PoC [][]byte-based replacement logic
    Remove the unused (and worse) [][]byte-based implementation of
    the replace package in favor of the ([]byte, map[string]string)
    implementation.

--- internal/replace/replacer.go
+++ internal/replace/replacer.go
@@ -17,7 +17,6 @@ package replace
 
 import (
 	"bytes"
-	"unicode/utf8"
 
 	"github.com/ericcornelissen/stringsx"
 	"github.com/ericcornelissen/wordrow/internal/logger"
@@ -28,13 +27,6 @@ import (
 type mapping struct {
 	from string
 	to   string
-}
-
-// Mapping is a utility struct representing a single mapping `from` one string
-// `to` another.
-type byteMapping struct {
-	from []byte
-	to   []byte
 }
 
 // Get the replacement string including prefix/suffix given the match `m`.
@@ -96,81 +88,6 @@ func safeReplaceOne(s []byte, m *mapping) []byte {
 func All(s []byte, m map[string]string) []byte {
 	for from, to := range m {
 		s = safeReplaceOne(s, &mapping{from: from, to: to})
-	}
-
-	return s
-}
-
-// V -- Bytes implementation -- V //
-
-// Get the replacement string including prefix/suffix given the match `m`.
-func getReplacementBytes(m *match, s []byte) (replacement []byte) {
-	keepPrefix, keepSuffix := detectAffixBytes(s)
-
-	if keepPrefix {
-		replacement = append(replacement, m.prefix...)
-	}
-	replacement = append(replacement, s...)
-	if keepSuffix {
-		replacement = append(replacement, m.suffix...)
-	}
-
-	return replacement
-}
-
-// Replace all instances of `from` by `to` in `s`.
-func replaceOneBytes(s []byte, m *byteMapping) []byte {
-	var bb bytes.Buffer
-
-	lastIndex := 0
-	for match := range matchesBytes(s, m.from) {
-		replacement := getReplacementBytes(match, m.to)
-		replacement, offset := maintainFormattingBytes(match.full, replacement)
-
-		bb.Write(s[lastIndex:maxInt(match.start, lastIndex)])
-		bb.Write(replacement)
-		lastIndex = match.end + offset
-	}
-
-	if lastIndex < len(s) {
-		bb.Write(s[lastIndex:])
-	}
-
-	return bb.Bytes()
-}
-
-// Replace all instances of `from` by `to` defined b `m` in `s`, or return the
-// original string if the mapping is invalid.
-func safeReplaceOneBytes(s []byte, m *byteMapping) []byte {
-	if !utf8.Valid(m.from) {
-		logger.Warningf("Invalid character in mapping '%s'", m.from)
-		return s
-	}
-
-	cleanFrom := bytes.TrimSpace(removeAffixNotationByte(m.from))
-	cleanTo := bytes.TrimSpace(removeAffixNotationByte(m.to))
-	if len(cleanFrom) == 0 || len(cleanTo) == 0 {
-		logger.Warningf("Invalid mapping value '%s,%s'", m.from, m.to)
-		return s
-	}
-
-	return replaceOneBytes(s, m)
-}
-
-// All replaces substrings of `s` according to the mapping defined by `m`.
-func AllBytes(s []byte, m [][]byte) []byte {
-	for i := 0; i < len(m); i += 2 {
-		s = safeReplaceOneBytes(s, &byteMapping{from: m[i], to: m[i+1]})
-	}
-
-	return s
-}
-
-// All replaces substrings of `s` according to the mapping defined by `m`.
-func AllBytes2(s []byte, x ByteMap) []byte {
-	for i, from := range x.from {
-		to := x.to[i]
-		s = safeReplaceOneBytes(s, &byteMapping{from: from, to: to})
 	}
 
 	return s
